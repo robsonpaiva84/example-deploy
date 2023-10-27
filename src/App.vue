cdd2e581f144824ef1f248ed8f2566f7b33a8e17
robsonpaiva84 <149123580+robsonpaiva84@users.noreply.github.com>
2023-10-26 22:05:37 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/cep">Busca Endereco pelo CEP</RouterLink>
+        <RouterLink to="/cep">Busca por banana</RouterLink>
       </nav>
     </div>
   </header>
